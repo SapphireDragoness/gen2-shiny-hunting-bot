48fd88597cdfbe85b37d40b37537d7772fdf5d17
SapphireDragoness <linda.monfermoso@gmail.com>
2026-03-24 08:50:08 +0100

    more info for wild encounters

--- core/bot.py
+++ core/bot.py
@@ -2,23 +2,41 @@ from pyboy import PyBoy
 from pyboy.utils import WindowEvent
 from loguru import logger
 
+from utils.utils import read_stats, check_shiny, read_battle_info
+
 emu = PyBoy(
     "roms/crystal.gbc",
 )
+
+FLAG = False
 
 while not emu.send_input(WindowEvent.QUIT):
     emu.tick()
 
     # in a wild battle
     if hex(emu.memory[0xD22D]) == "0x1":
-        attack = hex(emu.memory[0xD20C])[2]
-        defense = hex(emu.memory[0xD20C])[3]
-        speed = hex(emu.memory[0xD20D])[2]
-        special = hex(emu.memory[0xD20D])[3]
+        # show pokemon info only once per battle
+        if not FLAG:
+            FLAG = True
+            info = read_battle_info(
+                species=emu.memory[0xD206],
+                level=emu.memory[0xD213],
+                location=emu.memory[0xDCB6],
+            )
 
-        logger.debug(f"Attack DV: {str(int(attack, 16))}")
-        logger.debug(f"Defense DV: {str(int(defense, 16))}")
-        logger.debug(f"Speed DV: {str(int(speed, 16))}")
-        logger.debug(f"Special DV: {str(int(special, 16))}")
+            stats = read_stats(emu.memory[0xD20C], emu.memory[0xD20D])
+
+            logger.debug(f"Attack DV: {stats["attack"]}")
+            logger.debug(f"Defense DV: {stats["defense"]}")
+            logger.debug(f"Speed DV: {stats["speed"]}")
+            logger.debug(f"Special DV: {stats["special"]}")
+
+            if check_shiny(stats):
+                logger.success("Shiny found!")
+                with open(
+                    f"save_states/{info["species"]}_{info["location"]}.state", "wb"
+                ) as f:
+                    emu.save_state()
     else:
         logger.debug("Not in a battle.")
+        FLAG = False
--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,42 @@
-ATK_VALUES = [2, 3, 6, 7, 10, 11, 14, 15]
+from utils.dicts import POKES
 
 
-def check_shiny(attack, speed, defense, special) -> bool:
-    if attack in ATK_VALUES and speed == 10 and defense == 10 and special == 10:
+def check_shiny(stats: dict) -> bool:
+    # from https://bulbapedia.bulbagarden.net/wiki/Shiny_Pok%C3%A9mon#Generation_II
+    ATK_VALUES = ["2", "3", "6", "7", "10", "11", "14", "15"]
+    if (
+        stats["attack"] in ATK_VALUES
+        and stats["speed"] == "10"
+        and stats["defense"] == "10"
+        and stats["special"] == "10"
+    ):
         return True
     return False
+
+
+def int_to_zeroed_hex(value: int) -> str:
+    val = str(hex(value))
+    # already 0xXX
+    if len(val) == 4:
+        return val
+    # 0xX -> 0x0X
+    return val[:2] + 0 + val[2:]
+
+
+def read_stats(d20c: int, d20d: int) -> dict:
+    return {
+        "attack": int_to_zeroed_hex(d20c)[2],
+        "defense": int_to_zeroed_hex(d20c)[3],
+        "speed": int_to_zeroed_hex(d20d)[2],
+        "special": int_to_zeroed_hex(d20d)[3],
+    }
+
+
+def read_battle_info(species: int, level: int, location: int) -> dict:
+    return {
+        "species": POKES[str(species)],
+        "level": str(level),
+        "location": str(
+            location
+        ),  # TODO: find out where the location is stored in RAM and map it in dicts.py
+    }
